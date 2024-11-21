0823cacc64b39c55d1960cdab57c36a8f2f47991
Nawar Ali <nr.ali1912@gmail.com>
2024-11-22 00:19:52 +0100

    modular monolith skelton

---
+++ src/API/Evently.Api/Program.cs
@@ -0,0 +1,25 @@
+using Evently.Api.Extensions;
+using Evently.Modules.Events.Api;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddEventModule(builder.Configuration);
+
+WebApplication app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+
+    app.ApplyMigration();
+}
+
+EventsModule.MapEndpoints(app);
+
+app.Run();
